f3d86174ad68dcf1e9040493cd2d3521ee0ed3f7
Kenec <nnamani.kenechukwu@gmail.com>
2017-07-04 19:40:44 +0100

    covert tests to ES6

--- test.js
+++ test.js
@@ -7,32 +7,32 @@ const Jeep = require("./Car.js").Jeep
 const Toyota = new Vehicle("Toyota","Camry",4,"Red");
 const PathFinder = new Jeep("PathFinder","2015",4,"Black",false,false);
 
-describe('Vehicle ', function () {
+describe('Vehicle ', ()  => {
 
     // Test if Toyota class
-    describe('Toyota', function () {
+    describe('Toyota', () => {
         it('should be an instance of the `Vehicle` class', function () {
           expect(Toyota).to.be.an.instanceof(Vehicle);
         });
-        it('should be a typeof object', function () {
+        it('should be a typeof object', ()  => {
           expect(typeof Toyota).to.equal(typeof {});
         });
     });
 
     // Test if PathFinder class
-    describe('PathFinder', function () {
+    describe('PathFinder', ()  => {
         it('should be an instance of the `Jeep` class', function () {
           expect(PathFinder).to.be.an.instanceof(Jeep);
         });
-        it('should be a typeof object', function () {
+        it('should be a typeof object', ()  => {
           expect(typeof PathFinder).to.equal(typeof {});
         });
-        it('should be a child of Vehicle class', function () {
+        it('should be a child of Vehicle class', ()  => {
           expect(PathFinder).to.be.an.instanceof(Vehicle);
         });
 
     });
 
-  
+
 
 });
